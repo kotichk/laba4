e49d9e458fe004e194526302b20c49ad7415a6e1
So <lps.2019@mail.ru>
2025-11-29 12:42:45 +0500

    Was add .gitignore

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 
 using namespace std;
 
+vector<string> readLine(string fileName) { //читает из текстового файла набор строк в вектор (функция № 1)
+    return vector<string>();
+}
+
+void printLine(vector<string> lines) { //выводит эти строки на экран (функция № 2)
+
+}
+
+void writeToFile(vector<string> lines, string fileName) { //записывает их файл (функция № 3)
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    vector<string> lines = readLine("inFile");
+    printLine(lines);
+    writeToFile(lines, "outFile.txt");
 }
