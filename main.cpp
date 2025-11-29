e87677bf9696637ccab9f15757a0f99c4097cdc9
Salnikova <salnikova-eka@yandex.ru>
2025-11-29 12:38:06 +0500

    feat: добавлены вызовы всех функций в main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "functions_2_3.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "\n Вызов функции 2" << endl;
+function2();
+
+cout << "\n Вызов функции 3" << endl;
+function3();
     return 0;
 }
