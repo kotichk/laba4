22471b1393eccbdce998de4e3fd4867770159171
So <lps.2019@mail.ru>
2025-11-29 13:17:43 +0500

    Function for read

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 
 using namespace std;
 
 vector<string> readLine(string fileName) { //читает из текстового файла набор строк в вектор (функция № 1)
-    return vector<string>();
+    vector<string> lines;
+    ifstream file(fileName);
+    string line;
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+    file.close();
+    cout << "Количество строк: " << lines.size() << endl;
+    return lines;
 }
 
 void printLine(vector<string> lines) { //выводит эти строки на экран (функция № 2)
@@ -20,7 +29,7 @@ void writeToFile(vector<string> lines, string fileName) { //записывает их файл (
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    vector<string> lines = readLine("inFile");
+    vector<string> lines = readLine("inFile.txt");
     printLine(lines);
     writeToFile(lines, "outFile.txt");
 }
